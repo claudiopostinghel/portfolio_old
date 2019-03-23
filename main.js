11de58995570e8519769bf082710020b4d35cd8a
Clauss <claudiopostinghel@gmail.com>
2019-03-23 12:31:24 +0100

    Uh...

--- main.js
+++ main.js
@@ -3,6 +3,56 @@
 
 $(document).ready(function () {
 
+    var isTour = false;
+
+    //open interest point description
+    $('.cd-single-point').children('a').on('click', function () {
+        var selectedPoint = $(this).parent('li');
+        if (selectedPoint.hasClass('is-open')) {
+            selectedPoint.removeClass('is-open').addClass('visited');
+        } else {
+            selectedPoint.addClass('is-open').siblings('.cd-single-point.is-open').removeClass('is-open').addClass('visited');
+        }
+    });
+
+
+
+    //close interest point description
+    $('.cd-close-info').on('click', function (event) {
+        event.preventDefault();
+        $(this).parents('.cd-single-point').eq(0).removeClass('is-open').addClass('visited');
+    });
+
+    //on desktop, switch from product intro div to product mockup div
+    // $('#cd-start').on('click', 
+    function tour() {
+        // event.preventDefault();
+        //detect the CSS media query using .cd-product-intro::before content value
+        var mq = window.getComputedStyle(document.querySelector('.cd-product-mockup'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+        if (mq == 'mobile') {
+            $('body,html').animate({ 'scrollTop': $($(this).attr('href')).offset().top }, 200);
+        } else {
+            $('.cd-product').addClass('is-product-tour').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('.cd-close-product-tour').addClass('is-visible');
+                $('.cd-points-container').addClass('points-enlarged').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function () {
+                    $(this).addClass('points-pulsing');
+                });
+            });
+        }
+        isTour = true;
+    };
+
+    //on desktop, switch from product mockup div to product intro div
+    // $('.cd-close-product-tour').on('click',
+    function untour() {
+        $('.cd-product').removeClass('is-product-tour');
+        $('.cd-close-product-tour').removeClass('is-visible');
+        $('.cd-points-container').removeClass('points-enlarged points-pulsing');
+        isTour = false;
+    };
+
+
+    // QUADRATINO
     var xxx = $('#xxx')
     var face = $('#face')
     var quadratino = $('#quadratino')
@@ -21,68 +71,63 @@ $(document).ready(function () {
     var ymouse = 0
 
     // MOUSEMOVE
-    $(document).mousemove(function (event) {
+    // $(document).mousemove(function (event) {
 
-        // MODIFICO VARIABILI MOUSE
-        xmouse = event.pageX
-        ymouse = event.pageY
+    //     // MODIFICO VARIABILI MOUSE
+    //     xmouse = event.pageX
+    //     ymouse = event.pageY
 
-        // FACCIA
-        var xposition = face.offset().left - $(window).scrollLeft()
-        var yposition = face.offset().top - $(window).scrollTop()
-        var projectheight = face.outerHeight()
-        var projectwidth = face.outerWidth()
-        var xdelta = xmouse - xposition - (projectwidth/2)
-        var ydelta = ymouse - yposition - (projectheight/2)
-        var distance = Math.sqrt( (xdelta*xdelta) + (ydelta*ydelta) )
+    //     // FACCIA
+    //     var xposition = face.offset().left - $(window).scrollLeft()
+    //     var yposition = face.offset().top - $(window).scrollTop()
+    //     var projectheight = face.outerHeight()
+    //     var projectwidth = face.outerWidth()
+    //     var xdelta = xmouse - xposition - (projectwidth / 2)
+    //     var ydelta = ymouse - yposition - (projectheight / 2)
+    //     var distance = Math.sqrt((xdelta * xdelta) + (ydelta * ydelta))
 
-        // CALCOLO ANGOLO
-        var coseno = xdelta / distance
-        var angolo
-        if (ydelta < 0) {
-            angolo = Math.acos( coseno )
-        } else {
-            angolo = Math.PI*2 - Math.acos( coseno )
-        }
-        angolo = angolo / (Math.PI*2) * 360
+    //     // CALCOLO ANGOLO
+    //     var coseno = xdelta / distance
+    //     var angolo
+    //     if (ydelta < 0) {
+    //         angolo = Math.acos(coseno)
+    //     } else {
+    //         angolo = Math.PI * 2 - Math.acos(coseno)
+    //     }
+    //     angolo = angolo / (Math.PI * 2) * 360
 
-        // CALCOLO IN QUADRATINO
-        if (distance < $(window).height()) {
-            quadratino
-            .css('visibility', 'visible')
-            .css('width', distance)
-            .css('top', yposition + projectheight/2)
-            .css('left', xposition + projectwidth/2)
-            .css('transform', 'rotate('+angolo*-1+'deg)')
+    //     // DISPLAY DELLE INFORMAZIONI
+    //     xxx.html(`
+    //             projectwidth: ${projectwidth} <br>
+    //             angolo: ${angolo} <br>
+    //                 `
+    //     )
 
-            if (xdelta > 0) {
-                face
-                .css('transform', 'scaleX(-1) rotate('+angolo+'deg)')
-            } else {
-                face
-                .css('transform', 'scaleX(-1) scaleY(-1) rotate('+angolo*-1+'deg)')
-            }
-        } else {
-            quadratino
-            .css('visibility', 'hidden')
+    //     // CALCOLO IN QUADRATINO
+    //     if (distance < $(window).height()) {
+    //         quadratino
+    //             .css('visibility', 'visible')
+    //             .css('width', distance)
+    //             .css('top', yposition + projectheight / 2)
+    //             .css('left', xposition + projectwidth / 2)
+    //             .css('transform', 'rotate(' + angolo * -1 + 'deg)')
 
-            face
-            .css('transform', 'rotate(0deg)')
-        }
+    //         if (xdelta > 0) {
+    //             face
+    //                 .css('transform', 'scaleX(-1) rotate(' + angolo + 'deg)')
+    //         } else {
+    //             face
+    //                 .css('transform', 'scaleX(-1) scaleY(-1) rotate(' + angolo * -1 + 'deg)')
+    //         }
+    //     } else {
+    //         quadratino
+    //             .css('visibility', 'hidden')
 
-        // DISPLAY DELLE INFORMAZIONI
-        xxx.html(`
-            x mouse: ${xmouse} <br>
-            y mouse: ${ymouse} <br>
-            x face: ${xposition} <br>
-            y face: ${yposition} <br>
-            x delta: ${ xdelta } <br>
-            y delta: ${ ydelta } <br>
-            distance: ${ distance } <br>
-            angolo: ${ angolo } <br>
-                `
-        )
-    })
+    //         face
+    //             .css('transform', 'rotate(0deg)')
+    //     }
+
+    // })
 
 
     var cloned = false;
@@ -98,6 +143,8 @@ $(document).ready(function () {
 
         cloned = $('#tesi')
             .clone()
+
+        cloned
             .attr('id', 'cloned')
             .css('position', 'fixed')
             .css('top', yposition)
@@ -105,27 +152,28 @@ $(document).ready(function () {
             .css('height', projectheight)
             .css('width', projectwidth)
 
-        //$('#mesection, #workssection').toggleClass('noshadow')
-
-        cloned.prependTo('body');
+        cloned.appendTo('body');
 
         tesi.css('opacity', '0')
 
-        setTimeout(() => {
-            cloned.addClass('fullscreen');
-        }, 100);
+        tour()
+
+        cloned.addClass('fullscreen');
 
         cloned.click(() => {
             cloned.toggleClass('fullscreen');
-            //$('#mesection, #workssection').toggleClass('noshadow')
-            
+            untour();
+
             setTimeout(() => {
                 cloned.remove()
                 cloned = false;
                 tesi.css('opacity', '1')
                 body.css('overflow', 'auto')
+
             }, 1000);
         })
+
     })
+
 
 })
